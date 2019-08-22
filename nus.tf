1657ab216df924bcb01ef53bc792edc7004e4c0d
kp-m10 <54301512+kp-m10@users.noreply.github.com>
2019-08-22 10:32:38 +0530

    Update nus.tf

--- nus.tf
+++ nus.tf
@@ -94,5 +94,5 @@ resource "aws_instance" "TestInstance"{
     instance_type   =   "${var.instance_type}"
     subnet_id   =   "${aws_subnet.public.id}"
     vpc_security_group_ids  =   ["${aws_security_group.SecG.id}"]
-    key_name    =   "leo"
+    key_name    =   "Key name"
 }
